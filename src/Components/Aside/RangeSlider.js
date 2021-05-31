49fc092a5c52de94519bcd62c1f9c03d905c2aee
ereklemik <ereklemikiashvili@gmail.com>
2021-05-31 11:42:25 +0400

    upadte

--- src/Components/Aside/RangeSlider.js
+++ src/Components/Aside/RangeSlider.js
@@ -5,20 +5,20 @@ const RangeSlider = ({ title, min, max, symbol, From, To }) => {
       <p className="range-slider__title">{title} Range</p>
       <input type="range" min={min} max={max} className="range-slider__input" />
       <div className="range-slider__scope-wrapper">
-        <div className="amount-from__wrapper">
+        <div className="price-from__wrapper">
           <div className="range-slider__symbol">
             <span className="symbol">{symbol}</span>
           </div>
-          <div className="range-slider__amount">
-            <span className="amount">{From}</span>
+          <div className="range-slider__scope">
+            <span className="scope">{From}</span>
           </div>
         </div>
-        <div className="amount-to__wrapper">
+        <div className="price-to__wrapper">
           <div className="range-slider__symbol">
             <span className="symbol">{symbol}</span>
           </div>
-          <div className="range-slider__amount">
-            <span className="amount">{To}</span>
+          <div className="range-slider__scope">
+            <span className="scope">{To}</span>
           </div>
         </div>
       </div>
